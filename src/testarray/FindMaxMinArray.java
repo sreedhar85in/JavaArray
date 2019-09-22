91780a7029b97d494b5e782d374ee8b9406f2d49
Sreedhar S <sreedhar85in@gmail.com>
2019-09-23 00:50:05 +0530

    Min Max array partial code change

---
+++ src/testarray/FindMaxMinArray.java
@@ -0,0 +1,22 @@
+package testarray;
+
+public class FindMaxMinArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int array1[] = {5,6,10,15,20,25,77,33,35,55};
+		
+		for (int i=0;i<=array1.length;i++) {
+			
+			for(int k:array1) {
+			
+				int y = k;
+				
+			
+			}
+			
+		}
+	}
+
+}
